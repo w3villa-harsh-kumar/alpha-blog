5395510066a3c89bb5cf81e5315d3a5182fe6829
w3villa-harsh-kumar <harsh.kumar@w3villa.com>
2023-05-01 21:24:36 +0530

    Adding Home and About Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+
+  # Defines the about path route ("/about")
+  get "/about", to: "pages#about"
 end
